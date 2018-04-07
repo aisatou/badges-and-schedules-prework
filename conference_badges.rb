a2d6ef56176ced44448afae36929334d915fa437
Aisatou Diallo <diallo.aisatou@gmail.com>
2018-04-07 21:26:45 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,7 +5,9 @@ end
 def batch_badge_creator(names)
   badges = []
   
-  range = 0..name.size -1
+  range = 0..names.size -1
   for index in range
     badges.push("Hello, my name is #{names[index]}.")
+  end
+  return badges
 end
